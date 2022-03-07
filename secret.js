7cebb27921fbcfd48975b0dcf0b3e8d9f1a61f63
Patrick Ken Kalonde <39599487+Kalondepatrick@users.noreply.github.com>
2022-03-07 11:08:47 -0500

    Update secret.js

--- secret.js
+++ secret.js
@@ -1,4 +1,4 @@
 /**
 * Hold secret values to be .gitignore'd
 */
-var MY_API_KEY = "AAPKcce7aebf4dbc44cea08dc512205db00a24SIigcRysKtA3m9LHB5e8mekEJK5zna7ejir9uNK-jQi41v-3ou5kD-UHHHMdBB"
+var MY_API_KEY = "AAPK627d6d0cb212408ab0b890d8fc213b8azOpiyorFPU0xv4sKe7SH-0yEZPMys0aZMynPs3TGtRKIr0348feE1QvrRVupVmvO"
