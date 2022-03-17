8de2f8b972001d1c5e42090d77f7a1ff927d486e
Patrick Ken Kalonde <39599487+Kalondepatrick@users.noreply.github.com>
2022-03-17 02:13:40 -0500

    Update demo.js

--- demo.js
+++ demo.js
@@ -78,10 +78,10 @@ require(["esri/config",
         popupTemplate: popupDistricts,
         }); 
 	
-    var cycleMap = new WebTiledLayer({
-	    url: "https://tiles.arcgis.com/tiles/kCCq6u9VUnRxswS4/arcgis/rest/services/Ndirande-ortho/MapServer"
-        });
-        map.addLayer(cycleMap);
+    //var cycleMap = new WebTiledLayer({
+	 //   url: "https://tiles.arcgis.com/tiles/kCCq6u9VUnRxswS4/arcgis/rest/services/Ndirande-ortho/MapServer"
+        //});
+        //map.addLayer(cycleMap);
           
          /********************
          * Add Map Legend 
